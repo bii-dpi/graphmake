8a32670a2171bc99518d860f7d8c59762e7ea84f
bii-dpi <>
2022-08-02 03:57:26 +0800

    ..

--- 03_whittle_pockets.py
+++ 03_whittle_pockets.py
@@ -16,6 +16,20 @@ def get_dist_fn(protein_coord):
     return lambda row: np.linalg.norm(row - protein_coord)
 
 
+def is_pocket_index(pair):
+    protein_coord, ligand_coords = pair
+    all_dists = np.apply_along_axis(get_dist_fn(protein_coord),
+                                    1,
+                                    ligand_coords)
+
+    return np.min(all_dists) <= 6
+
+
+def is_pocket_index_batch(pair_batch):
+
+    return [is_pocket_index(pair) for pair in pair_batch]
+
+
 def save_protein_pocket(pdb_id, cutoff=6):
     protein_coords = pd.read_pickle(f"proc_proteins/{pdb_id}.pkl")
     protein_coords = [coord[:-1] for coord in protein_coords]
@@ -26,13 +40,19 @@ def save_protein_pocket(pdb_id, cutoff=6):
                      for coord in sublist]
     ligand_coords = np.array(ligand_coords)
 
-    pocket_indices = []
-    for i, protein_coord in progressbar(list(enumerate(protein_coords))):
-        all_dists = np.apply_along_axis(get_dist_fn(protein_coord),
-                                        1,
-                                        ligand_coords)
-        if np.min(all_dists) <= cutoff:
-            pocket_indices.append(i)
+    protein_batches = np.array_split(protein_coords, 75)
+    pair_batches = []
+    for protein_batch in protein_batches:
+        pair_batches.append([(protein_coord, ligand_coords)
+                             for protein_coord in protein_batch])
+
+    with PPE() as executor:
+        are_pocket_indices_batched = \
+            executor.map(is_pocket_index_batch, pair_batches)
+    are_pocket_indices = [is_pocket_index
+                          for batch in are_pocket_indices_batched
+                          for is_pocket_index in batch]
+    pocket_indices = np.where(are_pocket_indices)[0]
 
     with open(f"proc_proteins/{pdb_id}_pocket.pkl", "wb") as f:
         protein_coords = pd.read_pickle(f"proc_proteins/{pdb_id}.pkl")
@@ -41,6 +61,6 @@ def save_protein_pocket(pdb_id, cutoff=6):
 
 
 if __name__ == "__main__":
-    with PPE() as executor:
-        executor.map(save_protein_pocket, pdb_ids)
+    for pdb_id in progressbar(pdb_ids):
+        save_protein_pocket(pdb_id)
 
